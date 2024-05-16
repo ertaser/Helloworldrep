2a629eab0837fbafececa53e1ac5781c04d3a1ba
ertaser <8zunturk8@gmail.com>
2024-05-16 17:36:12 +0500

    Commit
    commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import Helloworld.Helloworld;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new Helloworld("Hello, ", "world!");
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/Helloworld/Helloworld.java
@@ -0,0 +1,12 @@
+package Helloworld;
+
+public class Helloworld {
+
+    /**
+     * Hello
+     */
+    public Helloworld(String hello,String world){
+        System.out.print(hello);
+        System.out.println(world);
+    }
+}
